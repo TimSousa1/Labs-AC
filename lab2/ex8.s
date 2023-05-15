d7c5cc06f74efbfeacfb0518ffa20d04d6b153d1
TimSousa <martimnevessousa@gmail.com>
2023-05-15 23:29:13 +0100

    ex8

---
+++ lab2/ex8.s
@@ -0,0 +1,14 @@
+# the corresponding temporary registers
+# should get stored on the stack
+
+.text
+# storing the registers
+sw    t3, 0(sp)
+sw    t4, 4(sp)
+sw    t5, 8(sp)
+sw    t6, 12(sp)
+# loading them back to the registers
+lw    t3, 0(sp)
+lw    t4, 4(sp)
+lw    t5, 8(sp)
+lw    t6, 12(sp)
